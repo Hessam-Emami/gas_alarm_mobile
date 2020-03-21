ca7b735c988ecea35957f390cf6527a4ee67eb54
Hessam Emami <hesam.emami9@gmail.com>
2020-03-21 16:10:00 +0430

    Implemented intro screen presentation logic(enabling bluetooth to proceed)

--- app/src/main/java/com/emami/android/toxicgasalarm/ui/intro/IntroFragment.kt
+++ app/src/main/java/com/emami/android/toxicgasalarm/ui/intro/IntroFragment.kt
@@ -1,5 +1,8 @@
 package com.emami.android.toxicgasalarm.ui.intro
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.emami.android.toxicgasalarm.MainNavigator
@@ -10,6 +13,8 @@ import kotlinx.android.synthetic.main.intro_fragment.*
 class IntroFragment : BaseFragment<Nothing>(Nothing::class.java), IntroNavigator {
     companion object {
         fun newInstance() = IntroFragment()
+        //RequestCode
+        private const val RC_ENABLE_BLUETOOTH = 10
     }
 
     override val layoutId: Int
@@ -17,13 +22,35 @@ class IntroFragment : BaseFragment<Nothing>(Nothing::class.java), IntroNavigator
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragment_intro_btn_scan.setOnClickListener { navigateToMainFragment() }
-
+        fragment_intro_btn_scan.setOnClickListener { enableBluetoothIntent() }
     }
 
     override fun navigateToMainFragment() {
-        if (requireActivity() is MainNavigator) {
-            (requireActivity() as MainNavigator).provideScreenNavigator().navigateToMainScreen()
+        val activity = requireActivity()
+        if (activity is MainNavigator) {
+            activity.provideScreenNavigator().navigateToMainScreen()
+        }
+    }
+
+    private fun enableBluetoothIntent() {
+        Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE).run {
+            startActivityForResult(this, RC_ENABLE_BLUETOOTH)
+        }
+    }
+
+    private fun handleBluetoothIntentResult(resultCode: Int) {
+        if (resultCode == Activity.RESULT_OK) {
+            navigateToMainFragment()
+        } else {
+            showToast("Sadly we can't proceed with your bluetooth turned off")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_ENABLE_BLUETOOTH) {
+            handleBluetoothIntentResult(resultCode)
         }
     }
 }
+
